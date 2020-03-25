793737e57fd49807971566e31e8af44d7aee292a
Alexander Kosin <samurnin@gmail.com>
2020-03-25 12:15:34 -0400

    added unit tests for multiply, subtract and divide operations

--- src/test/java/com/samurnin/calculator/CalculatorClientTest.java
+++ src/test/java/com/samurnin/calculator/CalculatorClientTest.java
@@ -19,4 +19,19 @@ public class CalculatorClientTest {
     public void when1add2_then3() {
         assertThat(calculatorClient.add(1, 2).getAddResult()).isEqualTo(3);
     }
+
+    @Test
+    public void when2multiply3_then6() {
+        assertThat(calculatorClient.multiply(2, 3).getMultiplyResult()).isEqualTo(6);
+    }
+
+    @Test
+    public void when5subtract1_then4() {
+        assertThat(calculatorClient.subtract(5, 1).getSubtractResult()).isEqualTo(4);
+    }
+
+    @Test
+    public void when6divide2_then3() {
+        assertThat(calculatorClient.divide(6, 2).getDivideResult()).isEqualTo(3);
+    }
 }
